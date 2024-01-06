c3e23cafb37454031667c066589f4379ec27f103
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-06 21:34:52 +0200

    Update pilot_streamlit.py

--- pilot_streamlit.py
+++ pilot_streamlit.py
@@ -7,6 +7,8 @@ Created on Sat Jan  6 21:23:05 2024
 
 
 import streamlit as st
+from PyPDF2 import PdfReader
+
 #from streamlit_extras.add_vertical_space import add_vertical_space
 
 
@@ -25,4 +27,27 @@ with st.sidebar:
     ''')
  #   add_vertical_space(5)
     st.write('Made with ❤️ by [Livia Ellen](https://liviaellen.com/portfolio)')
+
+
+
+
+def main():
+    st.header("Talk to your PDF 💬")
+    st.write("This app uses OpenAI's LLM model to answer questions about your PDF file. Upload your PDF file and ask questions about it. The app will return the answer from your PDF file.")
+
+    st.header("1. Pass your OPEN AI API KEY here")
+    v='demo'
+    openai_key=st.text_input("**OPEN AI API KEY**", value=v)
+    st.write("You can get your OpenAI API key from [here](https://platform.openai.com/account/api-keys)")
+
+    if openai_key==v:
+        openai_key=st.secrets["OPENAI_API_KEY"]
+    # if openai_key=='':
+    #     load_dotenv()
+    os.environ["OPENAI_API_KEY"] = openai_key
+
+    # upload a PDF file
+
+    st.header("2. Upload PDF")
+    pdf = st.file_uploader("**Upload your PDF**", type='pdf')
 
